0854cc3f5de287b6c9fecb27e5c926e57d7fe80c
Swithart <swithart2sweet@gmail.com>
2024-05-28 17:32:59 +0800

    update index

--- classes/database.php
+++ classes/database.php
@@ -19,4 +19,55 @@ class database{
           
     }
 
+    function signup($firstname, $lastname, $birthday, $sex, $username, $password) {
+        $con = $this->opencon();
+        
+        $query = $con->prepare("SELECT user_name FROM users WHERE user_name = ?");
+        $query->execute([$username]);
+        
+        $existingUser =  $query->fetch();
+        if ($existingUser) {
+            return false;
+        } else {
+            return $con->prepare("INSERT INTO users (user_firstname, user_lastname, user_birthday, user_sex, user_name, user_pass) VALUES (?,?,?,?,?,?)")->execute([$firstname, $lastname, $birthday, $sex, $username, $password]);
+        }
+    }
+
+    function check($username, $password) {
+
+            $con = $this->opencon();
+
+            $query = $con->prepare("SELECT * FROM users WHERE user_name = ?");
+            $query->execute([$username]);
+
+            $user = $query->fetch(PDO::FETCH_ASSOC);
+            if ($user && password_verify($password, $user['user_pass'])) {
+                return $user;
+            }
+            return false;
+    }
+
+    function view() {
+            $con = $this->opencon();
+            return $con->query("SELECT users.user_id, users.user_firstname, users.user_lastname, users.user_birthday, users.user_sex, users.user_name, users.user_profile_picture, CONCAT(user_address.city,', ', user_address.province) AS address from users INNER JOIN user_address ON users.user_id = user_address.user_id")->fetchAll();
+        }
+
+    function delete($id) {
+        try {
+            $con = $this->opencon();
+            $con->beginTransaction();
+
+            $query = $con->prepare("DELETE FROM user_address WHERE user_id = ?");
+            $query->execute([$id]);
+
+            $query2 = $con->prepare("DELETE FROM users WHERE user_id = ?");
+            $query2->execute([$id]);
+
+            $con->commit();
+            return true; 
+        } catch (PDOException $e) {
+            $con->rollBack();
+            return false;
+        }  
+    }  
 }
